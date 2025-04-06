34646d192c5a27092404e9d2863661757954edd6
ilhanmanzis <ilhanmanzis1207@gmail.com>
2025-04-06 21:47:05 +0700

    create user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,7 +28,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(1)->create();
+        //User::factory(1)->create();
 
         //User::factory()->create([
         //  'name' => 'Test User',
